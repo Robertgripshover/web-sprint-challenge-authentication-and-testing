4bc5c62cda13f4fcdbce5c5e21bc7343878a6270
Robert Gripshover <robertgrips@gmail.com>
2023-04-18 11:16:20 -0400

    m

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,35 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+
+const db = require('../data/dbConfig')
+
+const Auth = require('./auth/')
+
+const User = require('./users/users-model')
+
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+
+
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
+
+
+test('enviroment is testing', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
+}) //testing envirment is up and running
+
+describe('getAll', () => {
+  test('resolves all the users in the table', async () => {
+    const result = await User.getUsers()
+    console.log(result)
+  })
 })
