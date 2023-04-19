d866f34a28b77a2555f6b8c364e5f386c7040fb3
Robert Gripshover <robertgrips@gmail.com>
2023-04-19 13:13:58 -0400

    I moved the users-model into the auth model becuase I think it would work better

---
+++ api/auth/auth-model.js
@@ -0,0 +1,42 @@
+const db = require('../../data/dbConfig')
+
+function getUsers() {
+    return db('users')
+} 
+
+
+function find() {
+    return db('users').select('user_id', 'username', 'password') //<< remove password later
+}
+
+
+function findBy(filter) {
+    return db('users')
+    .select('id', 'username', 'password')
+    .where(filter)
+}
+
+
+function findById(id) {
+    return db('users')
+    .select('id', 'username')
+    .where('id', id).first()
+}
+
+
+async function add({ username, password }) {
+
+   
+        const [id] = await db('users').insert({username, password})
+        return findById(id)
+    
+}
+
+
+  module.exports = {
+    find,
+    findBy,
+    findById,
+    add,
+    getUsers
+}
